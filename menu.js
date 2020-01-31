e09d844282ca11859663ae2f62fecb88178ffd71
wen chi yu <kyotofu28@gmail.com>
2020-01-31 23:07:36 +0800

    all modify

--- menu.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $(".menu").on("click", function() {
-        $(".menubox").show();
-    })
-    $("#closebtn").on("click", function() {
-        $(".menubox").hide();
-    })
-})
